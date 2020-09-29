7600de58868c442f58220d19055cdadd789dfb64
aqx95 <angqx95@gmail.com>
2020-09-29 13:48:19 +0800

    relational gcn

--- model/RGCN.py
+++ model/RGCN.py
@@ -1,60 +1,26 @@
-from dgl.nn.pytorch import RelGraphConv
-from convnet.rgcn import BaseRGCN
+from dgl.nn import RelGraphConv
 import torch.nn as nn
-import torch
-import torch.nn.functional as F
 from model.MLP import MLPPredictor
 
-class EmbeddingLayer(nn.Module):
-    def __init__(self, num_nodes, h_dim):
-        super(EmbeddingLayer, self).__init__()
-        self.embedding = torch.nn.Embedding(num_nodes, h_dim)
+class RGCN(nn.Module):
+    def __init__(self, in_feat, hid_feat, out_feat, n_hidden_layers, num_rel):
+        super().__init__()
+        self.h_embedding = nn.Embedding(in_feat, hid_feat)
+        self.mlp = MLPPredictor(hid_feat, out_feat)
 
-    def forward(self, g, h, r, norm):
-        return self.embedding(h)
+        self.layers = nn.ModuleList([RelGraphConv(
+                                    hid_feat, hid_feat, num_rel, low_mem=True, dropout=0.4)
+                                    for _ in range(n_hidden_layers)])
 
-class RGCN(BaseRGCN):
-    def build_input_layer(self):
-        return EmbeddingLayer(self.num_nodes, self.h_dim)
+    def forward(self, g, triplets):
+        h = self.h_embedding(g.ndata['node_feat'])
+        e = g.edata['edge_feat']
+        for conv in self.layers:
+            h = conv(g, h, e)
 
-    def build_hidden_layer(self, idx):
-        act = F.relu if idx < self.num_hidden_layers - 1 else None
-        return RelGraphConv(self.h_dim, self.h_dim, self.num_rels, "bdd",
-                self.num_bases, activation=act, self_loop=True,
-                dropout=self.dropout)
-
-class LinkPredict(nn.Module):
-    def __init__(self, in_dim, h_dim, num_rels, num_bases=-1,
-                 num_hidden_layers=1, dropout=0, use_cuda=False, reg_param=0):
-        super(LinkPredict, self).__init__()
-        self.rgcn = RGCN(in_dim, h_dim, h_dim, num_rels, num_bases,
-                         num_hidden_layers, dropout, use_cuda)
-        self.reg_param = reg_param
-        self.mlp = MLPPredictor(h_dim, num_rels)
-        self.w_relation = nn.Parameter(torch.Tensor(num_rels, h_dim))
-        nn.init.xavier_uniform_(self.w_relation,
-                                gain=nn.init.calculate_gain('relu'))
-
-
-    def forward(self, g, h, r, norm, triplets):
-        embedding = self.rgcn.forward(g, h, r, norm)
         score = self.mlp(h, triplets)
         return score
-        # DistMult
-        # s = embedding[triplets[:,0]]
-        # r = self.w_relation[triplets[:,1]]
-        # o = embedding[triplets[:,2]]
-        # score = torch.sum(s * r * o, dim=1)
-        # return embedding, score.unsqueeze(dim=1)
-
-    def regularization_loss(self, embedding):
-        return torch.mean(embedding.pow(2)) + torch.mean(self.w_relation.pow(2))
 
     def get_loss(self, predictions, labels):
-        # triplets is a list of data samples (positive and negative)
-        # each row in the triplets is a 3-tuple of (source, relation, destination)
-        # predict_loss = F.binary_cross_entropy_with_logits(score, labels)
-        # reg_loss = self.regularization_loss(embed)
-        # return predict_loss + self.reg_param * reg_loss
         predict_loss = nn.CrossEntropyLoss()(predictions, labels)
         return predict_loss
