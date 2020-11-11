49c00da71b70399334a1da109045819627cf571f
aqx95 <angqx95@gmail.com>
2020-11-11 21:10:34 +0800

    RGCN with MLP

---
+++ model/RGCN_MLP.py
@@ -0,0 +1,26 @@
+from dgl.nn import RelGraphConv
+import torch.nn as nn
+from model.MLP import MLPPredictor
+
+class RGCN_mlp(nn.Module):
+    def __init__(self, in_feat, hid_feat, out_feat, n_hidden_layers, num_rel):
+        super().__init__()
+        self.h_embedding = nn.Embedding(in_feat, hid_feat)
+        self.mlp = MLPPredictor(hid_feat, out_feat)
+
+        self.layers = nn.ModuleList([RelGraphConv(
+                                    hid_feat, hid_feat, num_rel, low_mem=True, dropout=0.4)
+                                    for _ in range(n_hidden_layers)])
+
+    def forward(self, g, triplets):
+        h = self.h_embedding(g.ndata['node_feat'])
+        e = g.edata['edge_feat']
+        for conv in self.layers:
+            h = conv(g, h, e)
+
+        score = self.mlp(h, triplets)
+        return score
+
+    def get_loss(self, predictions, labels):
+        predict_loss = nn.CrossEntropyLoss()(predictions, labels)
+        return predict_loss
