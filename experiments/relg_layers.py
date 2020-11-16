692bd120ebe6122fba1f3b873e70caf46ab4e703
aqx95 <angqx95@gmail.com>
2020-11-16 21:57:38 +0800

    Comparing N conv layers

--- experiments/relg_layers.py
+++ experiments/relg_layers.py
@@ -5,6 +5,7 @@ import time
 import os
 import torch
 import numpy as np
+import argparse
 import random
 import yaml
 import matplotlib.pyplot as plt
@@ -29,101 +30,110 @@ def set_seed(seed):
     np.random.seed(seed) # for numpy pseudo-random generator
     torch.manual_seed(seed) # pytorch (both CPU and CUDA)
 
+def main(args):
+    config_path = 'config_' + args.dataset + '.yaml'
+    config = utils.load_config(config_path)
+    set_seed(config['train']['seed'])
+    if config['dataset']['data_name'] == 'fb15k-237':
+        train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('fb15k-237')
+    if config['dataset']['data_name'] == 'biokg':
+        train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-biokg")
+    if config['dataset']['data_name'] == 'wikikg':
+        train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-wikikg")
+    if config['dataset']['data_name'] == 'wn18':
+        train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('wn18')
 
-config = utils.load_config('config.yaml')
-set_seed(config['train']['seed'])
-if config['dataset']['data_name'] == 'fb15k-237':
-    train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('fb15k-237')
-if config['dataset']['data_name'] == 'biokg':
-    train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-biokg")
-if config['dataset']['data_name'] == 'wikikg':
-    train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-wikikg")
-if config['dataset']['data_name'] == 'wn18':
-    train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('wn18')
+    print('Training with {}'.format(config['dataset']['data_name']))
 
-print('Training with {}'.format(config['dataset']['data_name']))
+    # check cuda device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-# check cuda device
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    graph_data = DGLData(train_data, num_nodes, num_rels)
+    #Prep test
+    test_graph = graph_data.prep_test_graph()
+    test_labels = test_data[:,1].unsqueeze(dim=1)
+    #Prep validation
+    valid_labels = valid_data[:,1]
 
-graph_data = DGLData(train_data, num_nodes, num_rels)
-#Prep test
-test_graph = graph_data.prep_test_graph()
-test_labels = test_data[:,1].unsqueeze(dim=1)
-#Prep validation
-valid_labels = valid_data[:,1]
+    # Prepare model
+    relg1 = RELG_mlp(num_nodes,
+                in_dim_edge=num_rels,
+                hid_dim=config['model']['n_hidden'],
+                out_dim=config['model']['num_class'],
+                n_hidden_layers=1,
+                dropout=config['model']['dropout'],
+                graph_norm=True,
+                batch_norm=True,
+                residual=True)
 
-# Prepare model
-relg1 = RELG_mlp(num_nodes,
-            in_dim_edge=num_rels,
-            hid_dim=config['model']['n_hidden'],
-            out_dim=config['model']['num_class'],
-            n_hidden_layers=1,
-            dropout=config['model']['dropout'],
-            graph_norm=True,
-            batch_norm=True,
-            residual=True)
+    relg2 = RELG_mlp(num_nodes,
+                in_dim_edge=num_rels,
+                hid_dim=config['model']['n_hidden'],
+                out_dim=config['model']['num_class'],
+                n_hidden_layers=2,
+                dropout=config['model']['dropout'],
+                graph_norm=True,
+                batch_norm=True,
+                residual=True)
 
-relg2 = RELG_mlp(num_nodes,
-            in_dim_edge=num_rels,
-            hid_dim=config['model']['n_hidden'],
-            out_dim=config['model']['num_class'],
-            n_hidden_layers=2,
-            dropout=config['model']['dropout'],
-            graph_norm=True,
-            batch_norm=True,
-            residual=True)
+    relg3 = RELG_mlp(num_nodes,
+                in_dim_edge=num_rels,
+                hid_dim=config['model']['n_hidden'],
+                out_dim=config['model']['num_class'],
+                n_hidden_layers=3,
+                dropout=config['model']['dropout'],
+                graph_norm=True,
+                batch_norm=True,
+                residual=True)
 
-relg3 = RELG_mlp(num_nodes,
-            in_dim_edge=num_rels,
-            hid_dim=config['model']['n_hidden'],
-            out_dim=config['model']['num_class'],
-            n_hidden_layers=3,
-            dropout=config['model']['dropout'],
-            graph_norm=True,
-            batch_norm=True,
-            residual=True)
+    relg5 = RELG_mlp(num_nodes,
+                in_dim_edge=num_rels,
+                hid_dim=config['model']['n_hidden'],
+                out_dim=config['model']['num_class'],
+                n_hidden_layers=5,
+                dropout=config['model']['dropout'],
+                graph_norm=True,
+                batch_norm=True,
+                residual=True)
 
-relg5 = RELG_mlp(num_nodes,
-            in_dim_edge=num_rels,
-            hid_dim=config['model']['n_hidden'],
-            out_dim=config['model']['num_class'],
-            n_hidden_layers=5,
-            dropout=config['model']['dropout'],
-            graph_norm=True,
-            batch_norm=True,
-            residual=True)
+    model_zoo = [relg1, relg2, relg3, relg5]
+    epoch_count = range(1, config['train']['n_epochs'] + 1)
 
-model_zoo = [relg1, relg2, relg3, relg5]
-epoch_count = range(1, config['train']['n_epochs'] + 1)
+    labels = ['RELG1', 'RELG2', 'RELG3', 'RELG5']
 
-labels = ['RELG1', 'RELG2', 'RELG3', 'RELG5']
-
-## Training
-iter = 0
-for model in model_zoo:
-    print('Training with {}'.format(labels[iter]))
-    print('Number of parameters: {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
-    fitter = Fitter(model, config, device)
-    train_loss, val_loss = fitter.fit(graph_data, test_graph, valid_data, test_labels, valid_labels)
-    plt.plot(epoch_count, train_loss, label=labels[iter])
+    ## Training
+    iter = 0
+    for model in model_zoo:
+        print('Training with {}'.format(labels[iter]))
+        print('Number of parameters: {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+        fitter = Fitter(model, config, device)
+        train_loss, val_loss = fitter.fit(graph_data, test_graph, valid_data, test_labels, valid_labels)
+        plt.plot(epoch_count, train_loss, label=labels[iter])
 
 
-    ## Inference
-    model = model.to('cpu')
-    model.eval()
-    with torch.no_grad():
-        print("Evaluating...")
-        pred_test = model(test_graph, test_data)
-        print('Results for {}'.format(labels[iter]))
-        metrics.get_mrr(pred_test, test_labels, hits=[1,3,10])
+        ## Inference
+        model = model.to('cpu')
+        model.eval()
+        with torch.no_grad():
+            print("Evaluating...")
+            pred_test = model(test_graph, test_data)
+            print('Results for {}'.format(labels[iter]))
+            metrics.get_mrr(pred_test, test_labels, hits=[1,3,10])
 
-    iter += 1
+        iter += 1
 
-plt.savefig('loss.png', bbox_inches = "tight")
+    plt.savefig('loss.png', bbox_inches = "tight")
 
-plt.title('Training & Validation Loss on {}'.format(config['dataset']['data_name']))
-plt.xlabel('Epochs')
-plt.ylabel('Training Loss')
-plt.legend()
-plt.savefig('train_loss.png')
+    plt.title('Training & Validation Loss on {}'.format(config['dataset']['data_name']))
+    plt.xlabel('Epochs')
+    plt.ylabel('Training Loss')
+    plt.legend()
+    plt.savefig('train_loss.png')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RELG')
+    parser.add_argument("-d", "--dataset", type=str, required=True,
+            help="dataset to use")
+    args = parser.parse_args()
+    main(args)
