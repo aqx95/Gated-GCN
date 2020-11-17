8138e53fe16822ef716ebcaaeb3127c9ae69ee1e
aqx95 <angqx95@gmail.com>
2020-11-17 18:35:14 +0800

    Training scripts with negative sampling

---
+++ main_1.py
@@ -0,0 +1,114 @@
+import os
+import torch
+import random
+import yaml
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ogb_data import prepare_ogb
+from dgl_data import dgl_data
+from utilities import utils, metrics
+from trainer1 import Fitter
+from graphdata import DGLData
+
+from model.GATEDGCN import GatedGCN
+from model.RGCN import RGCN
+from model.RELG import RELG
+
+
+def set_seed(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
+def main(args):
+    config_path = 'config_' + args.dataset + '.yaml'
+    config = utils.load_config(config_path)
+
+    set_seed(2020)
+    if config['dataset']['data_name'] == 'fb15k-237':
+        train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('fb15k-237')
+    if config['dataset']['data_name'] == 'biokg':
+        train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-biokg")
+    if config['dataset']['data_name'] == 'wikikg':
+        train_data, valid_data, test_data, num_nodes, num_rels = prepare_ogb("ogbl-wikikg")
+    if config['dataset']['data_name'] == 'wn18':
+        train_data, valid_data, test_data, num_nodes, num_rels = dgl_data('wn18')
+
+    # Use available device (CPU/GPU)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # Preprocess Data
+    graph_data = DGLData(train_data, num_nodes, num_rels)
+    # Prepare test and validation graph
+    test_graph, test_node_id, test_rel = graph_data.prep_test_graph()
+    test_data = torch.LongTensor(test_data)
+    test_labels = test_data[:,1].unsqueeze(dim=1)
+    valid_labels = valid_data[:,1]
+
+    # Prepare Models
+    gated = GatedGCN(num_nodes,
+                    in_dim_edge=num_rels,
+                    hid_dim=config['model']['n_hidden'],
+                    out_dim=config['model']['n_hidden'],
+                    n_hidden_layers=config['model']['n_layers'],
+                    dropout=config['model']['dropout'],
+                    graph_norm=True,
+                    batch_norm=True,
+                    residual=True)
+
+    rgcn = RGCN(num_nodes,
+                config['model']['n_hidden'],
+                config['model']['n_hidden'],
+                config['model']['n_layers'],
+                num_rels)
+
+    relg = RELG(num_nodes,
+                in_dim_edge=num_rels,
+                hid_dim=config['model']['n_hidden'],
+                out_dim=config['model']['n_hidden'],
+                n_hidden_layers=config['model']['n_layers'],
+                dropout=config['model']['dropout'],
+                graph_norm=True,
+                batch_norm=True,
+                residual=True)
+
+    model_zoo = [gated, rgcn, relg]
+    epoch_count = range(1, config['train']['n_epochs'] + 1)
+
+    labels = ['GATED','RGCN','RELG']
+
+    # Training
+    iter = 0
+    for model in model_zoo:
+        print('Training with {}'.format(labels[iter]))
+        fitter = Fitter(model, config, device)
+        train_loss = fitter.fit(graph_data, test_graph, valid_data, valid_labels)
+        plt.plot(epoch_count, train_loss, label=labels[iter])
+
+        # Inference
+        if args.dataset not in ["fb15k", "wn18"]:
+            model = model.to('cpu')
+        model.eval()
+        print('Start evaluating...')
+        with torch.no_grad():
+            embed = model(test_graph, test_node_id, test_rel)
+            mrr = metrics.calc_mrr(embed, model.distmult, torch.LongTensor(train_data),
+                                 valid_data, test_data, hits=[1, 3, 10], eval_bz=args.eval_batch_size,
+                                 eval_p=args.eval_protocol)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RELG')
+    parser.add_argument("-d", "--dataset", type=str, required=True,
+                        help="dataset to use")
+    parser.add_argument("--eval-batch-size", type=int, default=500,
+            help="batch size when evaluating")
+    parser.add_argument("--eval-protocol", type=str, default="filtered",
+            help="type of evaluation protocol: 'raw' or 'filtered' mrr")
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ trainer1.py
@@ -0,0 +1,105 @@
+import os
+import numpy as np
+import torch.nn as nn
+import torch
+from glob import glob
+import dgl
+from tqdm import tqdm
+
+class Fitter:
+    def __init__(self, net, config, device):
+        self.model = net
+        self.config = config
+        self.device = device
+
+        self.base_dir = self.config['train']['folder']
+        if not os.path.exists(self.base_dir):
+            os.makedirs(self.base_dir)
+
+        self.best_loss = 10**5
+        self.hist_loss = []
+        self.val_loss = []
+        self.epoch = 0
+        self.log_path = f'{self.base_dir}/log.txt'
+
+        # Optimizer and scheduler
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['optimizer']['lr'],
+                                          weight_decay=config['optimizer']['regularization'])
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=50, gamma=0.1)
+
+        self.log('Begin training with {}'.format(self.device))
+        self.model = self.model.to(self.device)
+
+    def fit(self, graph_data, test_graph, valid_data, valid_labels):
+        self.hist_loss = []
+        for i in range(self.config['train']['n_epochs']):
+            train_loss = self.train_epoch(graph_data)
+            self.hist_loss.append(train_loss)
+            self.log(f'[TRAINING] Epoch {self.epoch}    Loss: {train_loss}')
+
+            # valid_loss = self.validate_epoch(test_graph, valid_data, valid_labels)
+            # self.val_loss.append(valid_loss)
+            # self.log(f'[VALIDATION] Epoch {self.epoch}    Loss: {valid_loss}')
+            #
+            # if self.best_loss > valid_loss:
+            #     self.best_loss = valid_loss
+            #     self.model.eval()
+            #     self.save(f'{self.base_dir}/best-checkpoint-epoch{str(self.epoch).zfill(2)}.bin')
+            #     for path in sorted(glob(f'{self.base_dir}/best-checkpoint-epoch*.bin'))[:-3]:
+            #         os.remove(path)
+            #
+            # self.scheduler.step(metrics=valid_loss)
+            self.scheduler.step()
+            self.epoch += 1
+
+        return self.hist_loss
+
+    def train_epoch(self, graph_data):
+        self.model = self.model.to(self.device)
+        self.model.train()
+        g, node_id, edge_type, data, labels = graph_data.prep_train_graph(self.config['graph_obj']['batch_size'],
+                                              self.config['graph_obj']['split_size'],
+                                              self.config['graph_obj']['neg_sampling'],
+                                              self.config['graph_obj']['edge_sampler'])
+
+        node_id, edge_type = node_id.to(self.device), edge_type.to(self.device)
+        g, data, labels = g.to(self.device), data.to(self.device), labels.to(self.device)
+        embeddings = self.model(g, node_id, edge_type)
+        train_loss = self.model.get_loss(embeddings, data, labels)
+
+        train_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config['optimizer']['grad_norm'])
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        del g, data, labels
+
+        return train_loss.detach().item()
+
+
+    def validate_epoch(self, test_graph, valid_data, valid_labels):
+        self.model = self.model.to('cpu')
+        self.model.eval()
+
+        with torch.no_grad():
+            valid_embed = self.model(test_graph, valid_data)
+            valid_loss = self.model.get_loss(valid_pred, valid_labels)
+
+        return valid_loss.detach().item()
+
+
+    def save(self, path):
+        self.model.eval()
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'scheduler_state_dict': self.scheduler.state_dict(),
+            'best_summary_loss': self.best_loss,
+            'epoch': self.epoch}, path)
+
+
+    def log(self, message):
+        if self.config['train']['verbose']:
+            print(message)
+        with open(self.log_path, 'a+') as logger:
+            logger.write(f'{message}\n')
